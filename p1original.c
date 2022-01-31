71bb1a1efe805af517313d21ddf8d89eef8e2ec3
Vikas6584 <69501909+Vikas6584@users.noreply.github.com>
2022-01-31 05:57:46 +0000

    completed p1 original and p1 debug log as well :))

--- p1original.c
+++ p1original.c
@@ -1,2 +1,29 @@
-fff
+#include <stdio.h>
+void input_two_numbers(int *a, int *b)
+{
+    printf("Enter two numbers\n");
+    scanf("%d%d",a,b); 
+}
+
+
+
+void find_sum(int a, int b, int *sum)
+{
+    *sum = a+b;
+}
+
+void output(int a, int b, int c)
+{
+    printf("Sum of %d + %d is %d\n",a,b,c);
+}
+
+int main()
+{
+    printf("Start Of The Program:\n");
+    int x,y,z;
+    input_two_numbers(&x,&y);
+    find_sum(x,y,&z);
+    output(x,y,z);
+    printf("End of Program: 🙏");
+    return 0;
 }
