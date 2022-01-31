ac3872c13b6b7f8c52080559237c49af1543bf06
Vikas6584 <69501909+Vikas6584@users.noreply.github.com>
2022-01-31 06:45:56 +0000

    completed p8 original and debug log of p8 as well :))

--- p7original.c
+++ p7original.c
@@ -1,59 +1,43 @@
 #include<stdio.h>
-typedef struct _complex
+typedef struct complex
 {
-  float real;
-  float imaginary;
-}Complex;
+    float real;
+    float imaginary;
+} complex;
 
-int get_n(){
-  int n;
-  printf("Enter the number of complex numbers: ");
-  scanf("%d", &n);
-  return n;
+complex add(complex n1, complex n2);
+
+complex input()
+{
+    complex c;
+    printf("Enter the Real part: ");
+    scanf("%f", &c.real);
+    printf("Enter the Imaginary part: ");
+    scanf("%f", &c.imaginary);
+    return c;
 }
 
-void input_n_complex(int n, Complex c[n]){
-  for(int i=0; i<n; i++){
-    printf("Enter the real part: ");
-    scanf("%f", &c[i].real);
-    printf("Enter the imaginary part: ");
-    scanf("%f", &c[i].imaginary);
-  }
+complex add(complex a, complex b)
+{
+    complex sum;
+    sum.real = a.real + b.real;
+    sum.imaginary = a.imaginary + b.imaginary;
+    return sum;
 }
 
-Complex add_n_complex(int n, Complex c[n]){
-  Complex res;
-  res.real = 0;
-  res.imaginary = 0;
-  for(int i=0; i<n; i++){
-    res.real += c[i].real; 
-    res.imaginary += c[i].imaginary;
-  }
-  return res;
+void output(complex a, complex b, complex sum)
+{
+    printf("The sum of %0.1f + %0.1fi and %0.1f + %0.1fi is %0.1f + %0.1fi\n", a.real, a.imaginary, b.real, b.imaginary, sum.real, sum.imaginary);
 }
 
-void output(int n, Complex c[n], Complex result){
-  if(n==1){
-      printf("%0.2f + %0.2fi is %0.2f + %0.2fi\n", c[0].real, c[0].imaginary, result.real, result.imaginary);
-  }else{
-      for(int i=0; i<n-1; i++){
-        printf("%0.2f + %0.2fi\n", c[i].real, c[i].imaginary);
-      }
-      printf("+ %0.2f + %0.2fi is\n%0.2f + %0.2fi\n", c[n-1].real, c[n-1].imaginary, result.real, result.imaginary);
-  }
 
-
-}
 
 int main()
 {
-  printf("Start Of The Program:\n");
-  int n = get_n();
-  Complex c[n], res;
-  input_n_complex(n, c);
-  res = add_n_complex(n, c);
-  output(n, c, res);
-  printf("\nEnd Of Code: 🙏");
-
-  return 0;
+    complex c1, c2, sum;
+    c1 = input();
+    c2 = input();
+    sum = add(c1, c2);
+    output(c1, c2, sum);
+    return 0;
 }
